9372792b4ae59ee87ed5bf0b92cf06851d8a0656
User <User@1-PC>
2024-11-30 20:15:38 +0500

    Добавьте файлы проекта.

---
+++ Product WPF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Product_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Product> _products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _products = new ObservableCollection<Product>();
+            ListBoxProduct.ItemsSource = _products;
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            string name = AddName.Text;
+            if (double.TryParse(AddPrice.Text, out double price) && int.TryParse(AddQuantity.Text, out int quantity))
+            {
+                Product product = new Product(name, price, quantity);
+                _products.Add(product);
+            }
+            else
+            {
+                MessageBox.Show("Неверный ввод");
+            }
+        }
+
+        private void AboutProduct_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBoxProduct.SelectedItem is Product selectedProduct)
+            {
+                var (name, price, quantity) = selectedProduct;
+                MessageBox.Show($"\nName: {name}\nPrice: {price:C}\nQuantity: {quantity}");
+            }
+            else
+            {
+                MessageBox.Show("Выберите сначало товар");
+            }
+        }
+    }
+}
---
+++ Product WPF/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_WPF
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public Product(string name, double price) : this(name, price, 0)
+        { }
+
+        public void Deconstruct(out string name, out double price, out int quantity)
+        {
+            name = Name;
+            price = Price;
+            quantity = Quantity;
+        }
+    }
+}
